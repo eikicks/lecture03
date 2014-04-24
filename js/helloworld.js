3cf73387d11687d5dcd334ffec45a28ccaae4e24
eikicks <eikicks@yhaoo.co.jp>
2014-04-24 12:36:35 +0900

    HalloJapan

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,11 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+var helloJapan = function(){
+	var message = document.getElementById("helloJapan");
+	message.setAttribute("class", "");
+};
+
+var showHelloJapanButton = document.getElementById("showHelloJapan");
+showHelloJapanButton.addEventListener("click", helloJapan);
